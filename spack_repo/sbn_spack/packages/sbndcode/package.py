28dd5b336a89b1996b186c23fdcdc24c5a1ec7d7
Patrick Gartung <gartung@fnal.gov>
2025-11-03 16:11:19 -0600

    Add catch2 to sbndcode

--- spack_repo/sbn_spack/packages/sbndcode/package.py
+++ spack_repo/sbn_spack/packages/sbndcode/package.py
@@ -79,6 +79,7 @@ class Sbndcode(CMakePackage):
     depends_on("fftw", type=("build", "run"))
     depends_on("hep-concurrency", type=("build", "run"))
     depends_on("messagefacility", type=("build", "run"))
+    depends_on("catch2", type=("build", "run"))
     depends_on("ifdh-art", type=("build", "run"))
     depends_on("tbb", type=("build", "run"))
     depends_on("geant4", type=("build", "run"))
